275816afa4998c6b19eb174df1c964de487ec0c1
ob82 <140307011+ob82@users.noreply.github.com>
2024-10-31 06:19:23 +0700

    Update game.js

--- game.js
+++ game.js
@@ -35,8 +35,8 @@ style.textContent = `
 document.head.appendChild(style);
 
 // Game constants
-const JUMP_FORCE = -6;
-const GRAVITY = 0.3;
+const JUMP_FORCE = -9;
+const GRAVITY = 0.2;
 const BOTTOM_TIME_LIMIT = 5000;
 
 // Scale factors based on screen size
